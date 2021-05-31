3fe95c8a28505913456bac36e2e6bcf148ad4914
tj0703 <tanvi.jain@aalto.fi>
2021-05-31 11:50:46 +0300

    all

---
+++ tests/test_search.py
@@ -0,0 +1,17 @@
+def test_basic_duckduckgo_search():
+    # Given the DuckDuckGo home page is displayed
+    # TODO
+
+    # When the user searches for "panda"
+    # TODO
+
+    # Then the search result title contains "panda"
+    # TODO
+
+    # And the search result query is "panda"
+    # TODO
+
+    # And the search result links pertain to "panda"
+    # TODO
+
+    raise Exception("Incomplete Test")
